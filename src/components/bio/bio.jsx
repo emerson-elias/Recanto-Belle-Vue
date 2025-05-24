7f4b55128f90baeec1ff9e321e27f74e56ad2a4c
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-05-24 17:28:36 -0300

    2.8

--- src/components/bio/bio.jsx
+++ src/components/bio/bio.jsx
@@ -1,9 +1,15 @@
 import './bio.scss'
 
-export default function Bio(){
-    return(
+export default function Bio() {
+    return (
         <section className="bio_container">
-
+            <h1>Repouse em um encanto e aconchego</h1>
+            
+            <p>Envolva-se em uma experiência onde o encanto encontra o aconchego.
+                Cada detalhe foi pensado para que você descanse com tranquilidade,
+                sinta-se em casa e viva momentos únicos em um ambiente acolhedor e inspirador.
+                Aqui, o tempo desacelera e o bem-estar é prioridade.</p>
+            <span><i class="fa-regular fa-gem"></i> Emerson Elias</span>
         </section>
     )
 }
