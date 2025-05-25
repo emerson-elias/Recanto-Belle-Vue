7ad983c6e1e02ce19e847b8ff89f0b7891022469
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-05-25 01:54:15 -0300

    3.4

--- src/components/video/video.jsx
+++ src/components/video/video.jsx
@@ -12,18 +12,15 @@ export default function Video() {
   return (
     <section className={styles.video_container}>
       <div className={styles.box_video}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
+
+        <svg xmlns="http://www.w3.org/2000/svg"
           width="512"
           height="512"
-          viewBox="0 0 26 26"
-          onClick={() => setShowModal(true)}
-          className={styles.play_button}
-        >
-          <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
-            <path d="m9 18.321l9.014-4.883L9 7.804zm9.49-4.003a1 1 0 0 0 .054-1.728L9.53 6.956A1 1 0 0 0 8 7.804v10.517a1 1 0 0 0 1.476.88z" />
-            <path d="M13 24.5c6.351 0 11.5-5.149 11.5-11.5S19.351 1.5 13 1.5S1.5 6.649 1.5 13S6.649 24.5 13 24.5m0 1c6.904 0 12.5-5.596 12.5-12.5S19.904.5 13 .5S.5 6.096.5 13S6.096 25.5 13 25.5" />
-          </g>
+          viewBox="0 0 48 48"
+          onClick={() => setShowModal(true)}>
+            
+          <circle cx="24" cy="24" r="21.5" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" />
+          <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M20.407 35L33.14 24L20.407 13" strokeWidth="1" />
         </svg>
 
         <video autoPlay loop muted playsInline className={styles.video_element}>
