45577ee29688e3625125fa0be12c933c8c1a1266
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-07-03 20:50:16 -0300

    9.2

--- src/components/home/services/services.jsx
+++ src/components/home/services/services.jsx
@@ -5,13 +5,15 @@ import styles from './services.module.scss'
 import adega from '/assets/img/beer.jpg'
 import buffet from '/assets/img/buffet.jpg'
 import services from '/assets/img/services.jpg'
+
 import Circle from '../../global/svg/circle'
 
 const service = [
     {
         id: 1,
         layer: 'layerOne',
-        title: 'Valle dos Vinhos',
+        title: 'adega vinhedo',
+        name: 'Valle dos Vinhos',
         description: 'Nossa adega oferece um ambiente sofisticado, com uma seleção especial de vinhos nacionais e importados. É o espaço ideal para quem deseja desfrutar de uma boa taça.',
         btnClass: 'btn',
         img: adega,
@@ -20,7 +22,8 @@ const service = [
     {
         id: 2,
         layer: 'layerTwo',
-        title: 'La Vue Gastronomia',
+        title: 'self service',
+        name: 'La Vue Gastronomia',
         description: 'Sabores únicos preparados com ingredientes frescos e regionais. Nosso buffet oferece uma verdadeira experiência gastronômica em um ambiente acolhedor.',
         btnClass: 'btn',
         img: buffet,
@@ -29,7 +32,8 @@ const service = [
     {
         id: 3,
         layer: 'layerThere',
-        title: 'Refúgio à Mesa',
+        title: 'serviço de quarto',
+        name: 'Refúgio à Mesa',
         description: 'Do atendimento personalizado às atividades de lazer, nossa pousada oferece serviços que transformam sua estadia em momentos inesquecíveis.',
         btnClass: 'btn',
         img: services,
@@ -41,7 +45,7 @@ function Services() {
     return (
         <section className={styles.services_container}>
 
-            {service.map(({ id, layer, title, description, btnClass, img, alt }) => (
+            {service.map(({ id, layer, name, title, description, btnClass, img, alt }) => (
 
                 <div key={id} className={styles[layer]}>
                     <div className={styles.boxOne}>
@@ -53,6 +57,7 @@ function Services() {
                     </div>
 
                     <div className={styles.boxTwo}>
+                        <h1>{name}</h1>
                         <img src={img} alt={alt} />
                     </div>
 
