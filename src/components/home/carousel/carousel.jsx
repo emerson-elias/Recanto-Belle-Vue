0864618fde3332ee80c774b2a49d5f3b893f4448
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-07-24 23:54:16 -0300

    14.0

--- src/components/home/carousel/carousel.jsx
+++ src/components/home/carousel/carousel.jsx
@@ -45,7 +45,7 @@ function Carousel() {
 
             <Swiper
                 modules={[Navigation, Pagination, Autoplay, EffectFade]}
-                effect="fade"
+                
                 fadeEffect={{ crossFade: false }}
                 slidesPerView={1}
                 pagination={{ type: 'progressbar' }}
