d6a016856e49493f9e61c9e250742a16a0616844
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2025-07-31 14:32:37 -0300

    16.2

--- src/components/suites/cards/cards.jsx
+++ src/components/suites/cards/cards.jsx
@@ -8,6 +8,8 @@ import 'swiper/css/navigation'
 
 import './cards.scss'
 
+import Title from '../../global/title/title'
+
 function Cards({ suites, onSelect }) {
     const navigate = useNavigate()
 
@@ -18,37 +20,46 @@ function Cards({ suites, onSelect }) {
 
     return (
         <section className='suites_cards'>
-            <div className='cards_title'>
-                <h1>Para todos:<br />outros requintes</h1>
-                <p>Nossas suítes combinam conforto e sofisticação para todos os gostos. Escolha a sua e desfrute de um merecido descanso.</p>
+
+            <Title
+                title={'Outras Acomodações'}
+                text={'Disponibilizamos outras opções para seu requinte e bem estar'}
+            />
+
+            <div className='row'>
+                <div className='boxOne'>
+                    <h1>Para todos:<br />outros requintes</h1>
+                    <p>Nossas suítes combinam conforto e sofisticação para todos os gostos. Escolha a sua e desfrute de um merecido descanso.</p>
+                </div>
+
+                <div className='boxTwo'>
+                    <Swiper
+                        slidesPerView={2}
+                        breakpoints={{
+                            0: { slidesPerView: 1 },
+                            700: { slidesPerView: 2 }
+                        }}
+                        centeredSlides={false}
+                        spaceBetween={50}
+                        loop={true}
+                        autoplay={{
+                            delay: 3000,
+                            disableOnInteraction: false
+                        }}
+                        modules={[Navigation, Autoplay]}
+                    >
+                        {suites.map((suite) => (
+                            <SwiperSlide key={suite.id} onClick={() => select(suite)}>
+                                <div className='card' >
+                                    <h1>{suite.categoria}</h1>
+                                    <img src={suite.banner} alt={suite.title} />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
             </div>
 
-            <div className='boxSwiper'>
-                <Swiper
-                    slidesPerView={2}
-                    breakpoints={{
-                        0: { slidesPerView: 1 },
-                        700: { slidesPerView: 2 }
-                    }}
-                    centeredSlides={false}
-                    spaceBetween={50}
-                    loop={true}
-                    autoplay={{
-                        delay: 3000,
-                        disableOnInteraction: false
-                    }}
-                    modules={[Navigation, Autoplay]}
-                >
-                    {suites.map((suite) => (
-                        <SwiperSlide key={suite.id} onClick={() => select(suite)}>
-                            <div className='card' >
-                                <h1>{suite.categoria}</h1>
-                                <img src={suite.banner} alt={suite.title} />
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
         </section>
     )
 }
